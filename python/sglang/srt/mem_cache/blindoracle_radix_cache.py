91655a8b63f85ad1e1e674ece923ba67a87daf7a
Natureal <naturechenpeng@gmail.com>
2025-05-29 21:08:57 +0800

    update

--- python/sglang/srt/mem_cache/blindoracle_radix_cache.py
+++ python/sglang/srt/mem_cache/blindoracle_radix_cache.py
@@ -356,17 +356,20 @@ class BlindOracleRadixCache(BasePrefixCache):
 
     def _match_prefix_helper(self, node: TreeNode, key: List):
         node.last_access_time = time.monotonic()
-
+        self._predictor_access(node)
         child_key = self.get_child_key_fn(key)
 
         value = []
         while len(key) > 0 and child_key in node.children.keys():
             child = node.children[child_key]
             child.last_access_time = time.monotonic()
+            self._predictor_access(child)
             prefix_len = self.key_match_fn(child.key, key)
             if prefix_len < len(child.key):
                 new_node = self._split_node(child.key, child, prefix_len)
                 value.append(new_node.value)
+                self._predictor_access(node)
+                self._predictor_access(new_node)
                 node = new_node
                 break
             else:
@@ -406,7 +409,7 @@ class BlindOracleRadixCache(BasePrefixCache):
         node.last_access_time = time.monotonic()
         if len(key) == 0:
             return 0
-        self._predictor_access(node)
+        #self._predictor_access(node)
 
         child_key = self.get_child_key_fn(key)
 
@@ -414,8 +417,7 @@ class BlindOracleRadixCache(BasePrefixCache):
         while len(key) > 0 and child_key in node.children.keys():
             node = node.children[child_key]
             node.last_access_time = time.monotonic()
-            self._predictor_access(node)
-            self.predictor.access(hash(tuple(node.key)))
+            #self._predictor_access(node)
             prefix_len = self.key_match_fn(node.key, key)
             total_prefix_length += prefix_len
             key = key[prefix_len:]
