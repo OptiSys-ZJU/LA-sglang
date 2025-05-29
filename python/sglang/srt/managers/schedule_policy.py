ea1047385e255106aa1cf7f7c60fac8bbd907bf8
Natureal <naturechenpeng@gmail.com>
2025-05-29 13:58:43 +0800

    test

--- python/sglang/srt/managers/schedule_policy.py
+++ python/sglang/srt/managers/schedule_policy.py
@@ -81,8 +81,8 @@ class SchedulePolicy:
         self.enable_hierarchical_cache = enable_hierarchical_cache
 
         # It is used to find the matching prefix for in-batch prefix caching.
-        #self.waiting_queue_radix_tree = BlindOracleRadixCache(
-        self.waiting_queue_radix_tree = RadixCache(
+        self.waiting_queue_radix_tree = BlindOracleRadixCache(
+        #self.waiting_queue_radix_tree = RadixCache(
             req_to_token_pool=None,
             token_to_kv_pool_allocator=None,
             page_size=1,
