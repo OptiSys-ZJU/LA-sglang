ce0abf077d8ddf6211b482b1fbfb278603fc0cfb
Natureal <naturechenpeng@gmail.com>
2025-05-29 12:09:40 +0800

    test

--- python/sglang/srt/model_executor/model_runner.py
+++ python/sglang/srt/model_executor/model_runner.py
@@ -847,7 +847,7 @@ class ModelRunner:
             )
 
         #self.max_total_num_tokens = self.profile_max_num_token(total_gpu_memory)
-        self.max_total_num_tokens = 100000
+        self.max_total_num_tokens = 20000
 
         if max_num_reqs is None:
             max_num_reqs = min(
