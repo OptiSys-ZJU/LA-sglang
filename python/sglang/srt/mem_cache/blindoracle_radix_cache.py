a80cfc331782cd3cb60ca479a1d5e897ff61dc5d
Natureal <naturechenpeng@gmail.com>
2025-06-06 12:37:49 +0800

    update

--- python/sglang/srt/mem_cache/blindoracle_radix_cache.py
+++ python/sglang/srt/mem_cache/blindoracle_radix_cache.py
@@ -122,9 +122,9 @@ class BlindOracleRadixCache(BasePrefixCache):
         self.disable = disable
         self.enable_kv_cache_events = enable_kv_cache_events
         self.kv_event_queue = []
-        self.predictor = POPUPredictor()
+        #self.predictor = POPUPredictor()
         #self.predictor = PLECOPredictor()
-        #self.predictor = LRBReuseDistancePredictor()
+        self.predictor = LRBReuseDistancePredictor()
 
         if self.token_to_kv_pool_allocator:
             self.device = self.token_to_kv_pool_allocator.device
