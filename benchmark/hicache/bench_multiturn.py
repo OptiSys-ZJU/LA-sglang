55f7816c21ca1627a60c2fa2139ae432a7ae8be5
Natureal <naturechenpeng@gmail.com>
2025-06-07 16:11:34 +0800

    update

--- benchmark/hicache/bench_multiturn.py
+++ benchmark/hicache/bench_multiturn.py
@@ -23,7 +23,7 @@ from sglang.bench_serving import (
 )
 
 #request_rate_list = [16, 14, 12, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
-request_rate_list = [1, 0.7, 0.4, 0.2, 0.1]
+request_rate_list = [1, 0.5, 0.2, 0.1, 0.05]
 request_rate_map = {}
 client_id_to_idx = {}
 idx_to_client_id = {}
