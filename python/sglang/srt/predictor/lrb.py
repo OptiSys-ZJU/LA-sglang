d61bfc4fec0c84e9c1dc29256dc6029d652e68c8
Natureal <naturechenpeng@gmail.com>
2025-06-08 14:37:35 +0800

    update

--- python/sglang/srt/predictor/lrb.py
+++ python/sglang/srt/predictor/lrb.py
@@ -42,7 +42,7 @@ class LRBReuseDistancePredictor(ReuseDistancePredictor):
 
         # online training
         self.training_config = model_config['training']
-        self.training_interval = 2000
+        self.training_interval = 200
         self.training_accumu_num = 0
         self.training_window = 20000
         self.existing_online_training = 0
