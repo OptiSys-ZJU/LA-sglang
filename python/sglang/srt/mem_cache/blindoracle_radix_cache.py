158fbfd42a9bac7f8514d1ae7aa201ef7be6c5ed
Natureal <naturechenpeng@gmail.com>
2025-06-08 17:54:52 +0800

    update

--- python/sglang/srt/mem_cache/blindoracle_radix_cache.py
+++ python/sglang/srt/mem_cache/blindoracle_radix_cache.py
@@ -441,6 +441,7 @@ class BlindOracleRadixCache(BasePrefixCache):
             logger.info(f"node pred = {node.pred}, truth = {self.current_ts}, interval = {self.current_ts - node.last_access_ts}, node key = {hash(tuple(node.key))}")
         node.pred_valid = 0
 
+        logger.info(f"current ts: {self.current_ts}")
         if self.current_ts % 100 == 0:
             captured = self._capture_print()
             logger.info(f"---------------------------------------------------- tree structure: {captured}")
